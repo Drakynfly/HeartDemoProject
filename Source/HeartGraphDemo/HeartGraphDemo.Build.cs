e2d562bb14c4f0f267fb6758ee332ce22010dc2f
Guy Lundvall <47373221+Drakynfly@users.noreply.github.com>
2023-05-29 16:20:34 -0700

    Init third person project

---
+++ Source/HeartGraphDemo/HeartGraphDemo.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class HeartGraphDemo : ModuleRules
+{
+	public HeartGraphDemo(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput" });
+	}
+}
