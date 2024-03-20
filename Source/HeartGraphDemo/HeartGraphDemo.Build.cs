6e6170f8d0c4cff8d5022625512af55fa3515fe3
Drakynfly <47373221+Drakynfly@users.noreply.github.com>
2024-03-20 00:33:39 -0700

    Update plugins

--- Source/HeartGraphDemo/HeartGraphDemo.Build.cs
+++ Source/HeartGraphDemo/HeartGraphDemo.Build.cs
@@ -1,4 +1,4 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+// Copyright Guy (Drakynfly) Lundvall. All Rights Reserved.
 
 using UnrealBuildTool;
 
--- Source/HeartGraphDemo/HeartGraphDemo.h
+++ Source/HeartGraphDemo/HeartGraphDemo.h
@@ -1,5 +1,3 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+// Copyright Guy (Drakynfly) Lundvall. All Rights Reserved.
 
 #pragma once
-
-#include "CoreMinimal.h"
